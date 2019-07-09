f6bba5ed918fb64590f4df90ed9709e05a49eaed
lenovo <lenovo@DESKTOP-RMB1HPE>
2019-07-09 11:07:59 +0800

    update

--- src/main/java/com/haoyu/controller/AdminController.java
+++ src/main/java/com/haoyu/controller/AdminController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/sys/admin")
+@RequestMapping("/admin")
 public class AdminController {
 
     @RequestMapping("index.page")
